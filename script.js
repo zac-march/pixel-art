a33ab3d4c789e87935c26fc5b9e598fca4ed4c9c
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-01-29 09:28:15 +1100

    Make Canvas size static

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let canvasDiv = document.querySelector('#canvas');
 
-let dimensions = 8
+
+let dimensions = 70
+let pixelSize = canvasDiv.offsetWidth / dimensions
+
 for (i = 0; i < dimensions; i++){
     let row = document.createElement("div")
     row.className = "row"
     for (index = 0; index < dimensions; index++){
         let pixel = document.createElement("div")
         pixel.className = "pixel"
+        pixel.style.width = `${pixelSize}px`;
+        pixel.style.height = `${pixelSize}px`;
         row.appendChild(pixel)
         
     }
