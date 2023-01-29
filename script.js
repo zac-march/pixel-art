eacbff7c7c1d10feaaff00d1c8788d3981542739
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-01-29 21:27:13 +1100

    Add canvas size label

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const canvasDiv = document.querySelector('#canvas');
 const canvasSizeBtns = document.querySelectorAll('.canvas-size-button');
+const canvasSizeLabel = document.querySelector('#canvas-size-label');
 let canvasSize = 16;
 const CANVAS_SIZE_VALUE = 8;
 const CANVAS_SIZE_MAX = 64;
@@ -7,9 +8,7 @@ const CANVAS_SIZE_MIN = 8;
 
 canvasDiv.addEventListener('mousedown', function(e){ clickPixel(e); isMouseDown = true; });
 canvasDiv.addEventListener('mouseup', function(){ isMouseDown = false;})
-canvasDiv.addEventListener('mouseover', function(e){ 
-    isMouseDown ? clickPixel(e) : false;
-})
+canvasDiv.addEventListener('mouseover', function(e){ isMouseDown ? clickPixel(e) : false;})
 
 
 canvasSizeBtns.forEach((button) => {
@@ -19,7 +18,7 @@ canvasSizeBtns.forEach((button) => {
             canvasSize -= CANVAS_SIZE_VALUE
             loadCanvas(canvasSize)
         }
-        if (btnText == 'Larger' && canvasSize != CANVAS_SIZE_MAX) {
+        else if (btnText == 'Larger' && canvasSize != CANVAS_SIZE_MAX) {
             canvasSize += CANVAS_SIZE_VALUE
             loadCanvas(canvasSize)
         }
@@ -32,6 +31,7 @@ let isMouseDown = false
 loadCanvas(canvasSize);
 
 function loadCanvas(dimensions) {
+    canvasSizeLabel.textContent = `${dimensions}x${dimensions}`
     canvasDiv.innerHTML = ''
     canvasDiv.style.gridTemplateColumns = `repeat(${dimensions}, 1fr)`;
     for (i = 0; i < dimensions * dimensions; i++) {
