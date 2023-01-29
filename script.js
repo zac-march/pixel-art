dbf3c89fa7eb7bb9a40a226121b36f888ab3228b
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-01-29 14:36:22 +1100

    Add ability to colour pixels on mouse over

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 let canvasDiv = document.querySelector('#canvas');
+let isMouseDown = false
 
 
-let dimensions = 10
+let dimensions = 50
 let pixelSize = canvasDiv.offsetWidth / dimensions
 canvasDiv.style.gridTemplateColumns = `repeat(${dimensions}, 1fr)`
 for (i = 0; i < dimensions * dimensions; i++){
     let pixel = document.createElement("div")
     pixel.className = "pixel"
-    //pixel.style.height = `${pixelSize}px`
+    //pixel.addEventListener('mousedown', function(e){ clickPixel(e) });
     canvasDiv.appendChild(pixel)
+}
+canvasDiv.addEventListener('mousedown', function(e){ console.log(e); isMouseDown = true; });
+canvasDiv.addEventListener('mouseup', function(){ isMouseDown = false;})
+canvasDiv.addEventListener('mouseover', function(e){ 
+    if (isMouseDown){
+        clickPixel(e)
+    }
+
+})
+
+
+function clickPixel(e){
+    if (e.target.id != 'canvas'){
+        e.target.style.backgroundColor = "red"
+    }
+
+    
 }
