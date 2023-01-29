c1b4483eef1b26683035184e92bd2862010657f7
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-01-30 07:53:25 +1100

    Add styling for colour pallette

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const canvasDiv = document.querySelector('#canvas');
 const canvasSizeBtns = document.querySelectorAll('.canvas-size-button');
 const canvasSizeLabel = document.querySelector('#canvas-size-label');
+const colourOptionDiv = document.querySelector('#colour-options');
+const COLOUR_OPTIONS = ['black', 'red', 'green', 'blue', 'white', 'yellow', 'orange', 'purple']
 let canvasSize = 16;
 const CANVAS_SIZE_VALUE = 8;
 const CANVAS_SIZE_MAX = 64;
@@ -29,6 +31,7 @@ canvasSizeBtns.forEach((button) => {
 let isMouseDown = false
 
 loadCanvas(canvasSize);
+loadColourOptions();
 
 function loadCanvas(dimensions) {
     canvasSizeLabel.textContent = `${dimensions}x${dimensions}`
@@ -38,6 +41,17 @@ function loadCanvas(dimensions) {
         let pixel = document.createElement("div");
         pixel.className = "pixel";
         canvasDiv.appendChild(pixel);
+    }
+}
+
+function loadColourOptions() {
+    numberOfColours = COLOUR_OPTIONS.length 
+    colourOptionDiv.style.gridTemplateColumns = `repeat(${numberOfColours / 2}, 1fr)`;
+    for (i = 0; i < numberOfColours; i++) {
+        let colourOption = document.createElement("div");
+        colourOption.style.backgroundColor = COLOUR_OPTIONS[i]
+        console.log(colourOption)
+        colourOptionDiv.appendChild(colourOption);
     }
 }
 
