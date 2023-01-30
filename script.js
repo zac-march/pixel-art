63af4a5b79c8c13655fd4bcf3699601cb5648fda
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-01-31 07:29:17 +1100

    Fix bugs in the colour palette

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ const canvasDiv = document.querySelector('#canvas');
 const canvasSizeBtns = document.querySelectorAll('.canvas-size-button');
 const canvasSizeLabel = document.querySelector('#canvas-size-label');
 const colourOptionDiv = document.querySelector('#colour-options');
-const COLOUR_OPTIONS = ['black', 'red', 'green', 'blue', 'white', 'yellow', 'orange', 'purple']
+const COLOUR_OPTIONS = ["black", "red", "orange", "yellow", "green", "blue", "white", "indigo", "violet", "pink", "purple", "brown"];
+
+
+
+
+
+
 let canvasSize = 16;
 const CANVAS_SIZE_VALUE = 8;
 const CANVAS_SIZE_MAX = 64;
@@ -12,27 +18,33 @@ const CANVAS_SIZE_MIN = 8;
 let selectedColour = COLOUR_OPTIONS[0];
 
 canvasDiv.addEventListener('mousedown', function(e){ 
-    if (e.target.className == 'pixel'){
+    if (e.target.className == 'pixel' && e.target.id != 'canvas'){
         clickPixel(e); 
         isMouseDown = true; 
     }
-    // else if (e.target.className == 'colour-option') {
-    //     console.log('hit')
-
-    // }
-    console.log(e)
 });
 canvasDiv.addEventListener('mouseup', function(){ isMouseDown = false;})
 canvasDiv.addEventListener('mouseover', function(e){ isMouseDown ? clickPixel(e) : false;})
 
+colourOptionDiv.addEventListener('click', function(e){
+    if (e.target.className == "colour-option"){
+        e.target.style.border = '2px solid #DADADA'
+        console.log(selectedColour)
+        colourOptions[COLOUR_OPTIONS.indexOf(selectedColour)].style.border = ''
+        selectedColour = e.target.style.backgroundColor
+        
+    }
+})
 
 canvasSizeBtns.forEach((button) => {
     button.addEventListener('click', (e) => {
         let btnText = e.target.textContent;
+
         if (btnText == 'Smaller' && canvasSize != CANVAS_SIZE_MIN) {
             canvasSize -= CANVAS_SIZE_VALUE
             loadCanvas(canvasSize)
         }
+
         else if (btnText == 'Larger' && canvasSize != CANVAS_SIZE_MAX) {
             canvasSize += CANVAS_SIZE_VALUE
             loadCanvas(canvasSize)
@@ -45,8 +57,8 @@ let isMouseDown = false
 
 loadCanvas(canvasSize);
 loadColourOptions();
-
 const colourOptions =  document.querySelectorAll('.colour-option');
+
 
 function loadCanvas(dimensions) {
     canvasSizeLabel.textContent = `${dimensions}x${dimensions}`
@@ -60,22 +72,17 @@ function loadCanvas(dimensions) {
 }
 
 function loadColourOptions() {
-    numberOfColours = COLOUR_OPTIONS.length 
+    numberOfColours = COLOUR_OPTIONS.length
     colourOptionDiv.style.gridTemplateColumns = `repeat(${numberOfColours / 2}, 1fr)`;
     for (i = 0; i < numberOfColours; i++) {
         let colourOption = document.createElement("div");
         colourOption.style.backgroundColor = COLOUR_OPTIONS[i]
         colourOption.className = "colour-option"
         colourOptionDiv.appendChild(colourOption);
-        colourOption.addEventListener('click', (e) =>{
-            selectedColour = COLOUR_OPTIONS[COLOUR_OPTIONS.indexOf(e.target.style.backgroundColor)]
-            console
-        })
     }
 }
 
+
 function clickPixel(e){
-    if (e.target.id != 'canvas'){
-        e.target.style.backgroundColor = selectedColour;
-    }
+    e.target.style.backgroundColor = selectedColour;
 }
